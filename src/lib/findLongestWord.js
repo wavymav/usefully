3923b0525579d7acb275a10bff9c953b6db64098
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-25 04:43:43 -0400

    added findTheLongest() to /src/lib

---
+++ src/lib/findLongestWord.js
@@ -0,0 +1,23 @@
+var findLongestWord = function(str) {
+  if (typeof(str) === 'string') {
+    var wordArray = str.split(' '),
+        previousWordLength = 0,
+        longestWord,
+        currentWordLength;
+
+    for (var i = 0; i < wordArray.length; i++) {
+      currentWordLength = wordArray[i].length;
+      if (currentWordLength > previousWordLength) {
+        previousWordLength = currentWordLength;
+        longestWord = wordArray[i];
+      }
+    }
+    return longestWord.length;
+  } else {
+    return undefined;
+  }
+};
+
+module.exports = {
+  longestWord: findLongestWord
+};
