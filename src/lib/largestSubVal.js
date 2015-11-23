3365ee4528c48824a69ddfff893781d23b34ab1b
Marcus Barnes <mavjbarnes@gmail.com>
2015-11-23 13:40:20 -0500

    added comments for largestSubVal()

--- src/lib/largestSubVal.js
+++ src/lib/largestSubVal.js
@@ -1,7 +1,20 @@
 /**
- * Takes in an
+ * Takes in a 2D array of numbers and returns an array of the largest value of
+ * each sub array. This is accomplished by first checking to see if the argument
+ * is in fact an Array by using Array.isArray() method since typeof would only
+ * return ‘object’. Next I set up the vars for the newArray, largestNumber, &
+ * currentNumber. Following that I used a nested forEach method on the array
+ * argument along with its sub array in order to iterate through each value.
+ * While this is being done, if statements are checking if each sub array is an
+ * array and each value in that sub array is a data type of number. Inside the
+ * sub array are if statement that check to see if the current value is > the
+ * comparison(previous value). Afterwards the largest number in that sub array
+ * will be pushed to the new array. When the outer forEach method on the array
+ * argument finally finish, a final if statement checks to see if the new array
+ * is not the same length as the array argument, if true the new array will be
+ * set to undefined, otherwise the new array is returned.
  * @param  {Array} arr - A 2D array of numbers
- * @return {Array}     - An array of numbers
+ * @return {Array}     - An array of the largest value of each sub array
  */
 export function largestSubVal(arr) {
   if (!Array.isArray(arr)) return undefined;
