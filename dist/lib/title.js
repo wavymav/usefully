94d4e97387cab216ddc83367d75456e6f885c089
Marcus Barnes <mavjbarnes@gmail.com>
2015-12-09 00:17:16 -0500

    updated /dist with title()

--- dist/lib/title.js
+++ dist/lib/title.js
@@ -3,21 +3,17 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.title = title;
 /**
- * This function first checks if argument is not a data type of string if false
- * program proceeds on. Declares an array & Initializes 2 variables. Next, the
- * string argument is spilt into an Array of words upon which the forEach method
- * is called to iterate through the array. With each word I grab the 0 index of
- * word and capitalize it. Following that I store the every after the 0 index
- * with the substr method and convert it to lowercase. Afterwards I concat and
- * push the two new strings to wordArr. Finally I join the wordArr back into a
- * string.
+ * @function - Will capitalize the first letter of each word in a given string
+ *           arguemnt.
+ *           
  * @param  {String} str - A simple string
  * @return {String}     - A Title Cased string
  */
-function title(str) {
-  if (typeof str !== 'string') return undefined;
+var title = exports.title = function title(str) {
+  if (typeof str !== 'string') {
+    throw new Error("Arguement must be of type string!");
+  }
   var wordArr = [];
   var firstLetter = undefined;
   var restOfWord = undefined;
@@ -28,7 +24,7 @@ function title(str) {
     wordArr.push(firstLetter + restOfWord);
   });
   return wordArr.join(' ');
-}
+};
 
 /*******************************************************************************
 
